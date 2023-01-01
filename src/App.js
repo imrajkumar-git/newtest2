3dd9ae6f4b16e8ad3295d19090ae3733954c0d1e
Rajkumar Aryal <80311912+imrajkumar-git@users.noreply.github.com>
2023-01-01 09:46:34 +0545

    Update App.js
    f

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Router from "./routing/Router";
+import React from 'react';
 
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+ 
 const App = () => {
   return (
     <>
       <Router />
+    <MessengerCustomerChat
+    pageId="100088834186081"
+    appId="458648586466110"
+  
+  />,
     </>
   );
 }
